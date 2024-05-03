08388098b7e22209de2b0568a127927970425cee
Evan Raphael Brown <evanraphaelbrown.erb@gmail.com>
2024-05-03 21:01:05 +0800

    Working code on owner

--- PetShop/Owner Page.xaml.cs
+++ PetShop/Owner Page.xaml.cs
@@ -24,6 +24,11 @@ namespace PetShop
     public partial class Owner_Page : Window
     {
         PetShopDataContext _PSDC = null;
+
+        public string Owners_FirstName { get; set; }
+        public string Owners_LastName { get; set; }
+        public string FullName => Owners_LastName + ", " + Owners_FirstName;
+
         public Owner_Page()
         {
             InitializeComponent();
@@ -33,7 +38,7 @@ namespace PetShop
                 (Properties.Settings.Default.Pet_Shop_DatabaseConnectionString);
 
             var OwnerData = from owners in _PSDC.Owners
-                        select owners.Owners_LastName + ", " + owners.Owners_FirstName;
+                            select owners.Owners_LastName + ", " + owners.Owners_FirstName;
             lbOwners.ItemsSource = OwnerData.ToList();
         }
 
@@ -63,16 +68,74 @@ namespace PetShop
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
         {
-            _PSDC.Proc_Add_Owner 
-                (tbOwnerID.Text, tbOwnerFName.Text, tbOwnerLName.Text, tbOwnerContact.Text, tbOwnerEmail.Text);
-
-            _PSDC.SubmitChanges();
-
             tbOwnerID.Text = null;
             tbOwnerFName.Text = null;
             tbOwnerLName.Text = null;
             tbOwnerContact.Text = null;
             tbOwnerEmail.Text = null;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var existingOwner = _PSDC.Owners.FirstOrDefault(o => o.Owners_ID == tbOwnerID.Text.ToString());
+
+            if (existingOwner != null)
+            {
+                // Update the existing owner's information
+                existingOwner.Owners_FirstName = tbOwnerFName.Text;
+                existingOwner.Owners_LastName = tbOwnerLName.Text;
+                existingOwner.Owners_ContactNumber = tbOwnerContact.Text;
+                existingOwner.Owners_Email = tbOwnerEmail.Text;
+
+                _PSDC.SubmitChanges();
+                MessageBox.Show("Owner information updated successfully.");
+            }
+            else
+            {
+                _PSDC.Proc_Add_Owner
+            (tbOwnerID.Text, tbOwnerFName.Text, tbOwnerLName.Text, tbOwnerContact.Text, tbOwnerEmail.Text);
+
+                _PSDC.SubmitChanges();
+            }
+        }
+
+        private void UpdateListView()
+        {
+            // Assuming Owners is the class representing your Owners table
+            var owners = _PSDC.Owners.ToList();
+
+            lbOwners.ItemsSource = owners;
+            lbOwners.DisplayMemberPath = "FullName"; // Display the last name in the ListBox
+        }
+
+        private void btViewing_Click(object sender, RoutedEventArgs e)
+        {
+            var OwnerData = from owners in _PSDC.Owners
+                            select owners.Owners_LastName + ", " + owners.Owners_FirstName;
+
+            lbOwners.ItemsSource = OwnerData.ToList();
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            lbOwners.ItemsSource = null;
+
+            string searchItem = tbSearch.Text;
+
+            var query = from entry in _PSDC.Owners
+                        where entry.Owners_LastName.Contains(searchItem)
+                           || entry.Owners_FirstName.Contains(searchItem)
+                        select entry;
+
+            List<string> ownerNames = new List<string>();
+            foreach (var entry in query)
+            {
+                // Add the formatted owner name to the list
+                ownerNames.Add(entry.Owners_LastName + ", " + entry.Owners_FirstName);
+            }
+
+            // Set the new list as the ItemsSource
+            lbOwners.ItemsSource = ownerNames;
         }
     }
 }
